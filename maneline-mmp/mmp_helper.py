11e1ba905ea535d98227449c111219ebfe1fe319
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-03-15 10:30:56 +0800

    Second example of usage of asyncio library

--- maneline-mmp/mmp_helper.py
+++ maneline-mmp/mmp_helper.py
@@ -15,4 +15,20 @@ async def main_v1():
 
     print(f"finished at {time.strftime('%X')}")
 
-asyncio.run(main_v1())
+async def main_v2():
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
+
+    task2 = asyncio.create_task(
+        say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main_v2())
