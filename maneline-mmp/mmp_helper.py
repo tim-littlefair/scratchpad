8dc99ea653b4aaf08b244b483467df0d7b094681
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-04-04 12:19:03 +0800

    WIP on eliminating exception during final GC

--- maneline-mmp/mmp_helper.py
+++ maneline-mmp/mmp_helper.py
@@ -64,14 +64,38 @@ class SubprocessTaskGroup(asyncio.TaskGroup):
     async def sp_terminate(self, tag, timeout=None):
         p = self.subprocess_map[tag]
         if p.returncode is not None:
+            print(f"{tag} has already exited")
             return
         if timeout is None:
             timeout = p.timeout_factor
-        try:
-            async with asyncio.timeout(timeout):
-                p.terminate()
-        except TimeoutError:
-            pass
+        # Different processes might require different signals to exit.
+        # We try the in this order:
+        # SIGTERM=15 (canonical polite request for process to end)
+        # SIGHUP=1   (simulates closure of input FD)
+        # SIGINT=2   (simulates ctrl-C on input)
+        # SIGQUIT=3  (simulates break on input)
+        # SIGKILL=9  (canonical mandatory process end signal)
+        for termination_signal in ( 15, 1, 2, 3, 9 ):
+            async with asyncio.timeout(p.timeout_factor*2):
+                try:
+                    print(f"Sending signal {termination_signal}")
+                    p.send_signal(termination_signal)
+                    await asyncio.sleep(p.timeout_factor)
+                    await p.wait()
+                    break
+                except TimeoutError:
+                    print(f"{tag} communication timed out")
+                except asyncio.exceptions.CancelledError:
+                    print(f"{tag} read cancelled")
+        status = await p.wait()
+        # We expect the status to be equal to the negation of the last signal sent
+        print(f"{tag} has exited with status {status}")
+        # Violating the privacy of p._transport to ensure that it is
+        # manually closed before garbage collection runs is a workaround for
+        # uncatchable exceptions generated on exit described in
+        # https://github.com/python/cpython/issues/114177
+        p._transport.close()
+
     async def wait_for_processes(self, timeout_factor=None):
         while len(self.subprocess_map)==0:
             print(f"Stopping process {k}",flush=True)
@@ -109,7 +133,7 @@ class SubprocessTaskGroup(asyncio.TaskGroup):
                             output = await self.sp_read(k)
                             print(f"Final output on {k}>>>\n{output}\n<<<")
                     except asyncio.exceptions.CancelledError:
-                        print(f"CNo final output on {k}")
+                        print(f"No final output on {k}")
                     except asyncio.exceptions.TimeoutError:
                         print(f"No final output on {k}")
                 p.stdout.close()
@@ -118,8 +142,12 @@ class SubprocessTaskGroup(asyncio.TaskGroup):
                 print(f"Shutdown of subprocess {k} raised unhandled exception and might still be running)")
                 traceback.print_exception(sys.exc_info()[1])
                 break
-            del self.subprocess_map[k]
-            del self.task_map[k]
+            try:
+                del self.task_map[k]
+                del self.subprocess_map[k]
+            except RuntimeError:
+                print("RE")
+                pass
 
 class ScriptRunner:
     def __init__(self, task_group):
@@ -155,21 +183,24 @@ class ScriptRunner:
                 break
 
 
-async def get_mmp_bdaddr(task_group, tag):
+async def get_mmp_bdaddr(task_group, btctl_tag, hdmp_tag):
     await asyncio.sleep(1.0)
-    await task_group.sp_write(tag,"\n") # for output clarity
-    await task_group.sp_write(tag,"scan.clear\n")
-    await task_group.sp_write(tag,"scan.pattern 'Mustang Micro Plus'\n")
-    await task_group.sp_write(tag,"scan le\n")
-    await asyncio.sleep(2.0)
-    await task_group.sp_write(tag,"scan off\n")
-    scan_output = await task_group.sp_read(tag,1.0)
-    bdaddr_pattern = re.compile(r"Device ([:\d\w]+) RSSI")
-    bdaddr_match = bdaddr_pattern.search(scan_output)
-    if bdaddr_match is not None:
-        return bdaddr_match.group(1)
-    else:
-        return None
+    await task_group.sp_write(btctl_tag,"\n") # for output clarity
+    await task_group.sp_write(btctl_tag,"scan.clear\n")
+    await task_group.sp_write(btctl_tag,"scan.pattern 'Mustang Micro Plus'\n")
+    await task_group.sp_write(btctl_tag,"scan le\n")
+    bd_addr = None
+    for _ in range(0,3):
+        await asyncio.sleep(2.0)
+        scan_output = await task_group.sp_read(hdmp_tag,2.0)
+        print(scan_output)
+        bdaddr_pattern = re.compile(r"Device ([:\d\w]+) RSSI")
+        bdaddr_match = bdaddr_pattern.search(scan_output)
+        if bdaddr_match is not None:
+            bd_addr = bdaddr_match.group(1)
+            break
+    await task_group.sp_write(btctl_tag,"scan off\n")
+    return bd_addr
 
 async def subscribe_for_gatt_replies(task_group, tag, bdaddr):
     await asyncio.sleep(0.1)
@@ -192,12 +223,14 @@ async def subscribe_for_gatt_replies(task_group, tag, bdaddr):
 
 async def mmp_main():
     _TAG_BTCTL = "btctl"
+    _TAG_HDMP = "hdmp"
     print(f"started at {time.strftime('%X')}")
     async with SubprocessTaskGroup() as tg:
 
         try:
-            btctl_task = await tg.create_subprocess_task(_TAG_BTCTL,"bluetoothctl")
-            bdaddr = await get_mmp_bdaddr(tg, _TAG_BTCTL)
+            btctl_task = await tg.create_subprocess_task(_TAG_BTCTL,"bluetoothctl --timeout 2")
+            hcidump_task = await tg.create_subprocess_task(_TAG_HDMP,"hcidump -X")
+            bdaddr = await get_mmp_bdaddr(tg, _TAG_BTCTL, _TAG_HDMP)
             if bdaddr is not None:
                 print(f"MMP bdaddr: {bdaddr}",flush=True)
             else:
@@ -214,10 +247,17 @@ async def mmp_main():
 
         print("Waiting for processes to exit")
         await tg.sp_terminate(_TAG_BTCTL)
+        await tg.sp_terminate(_TAG_HDMP)
+        gc.collect()
         await tg.wait_for_processes()
+        gc.collect()
 
     # The await is implicit when the context manager exits.
     print(f"finished at {time.strftime('%X')}")
 
 if __name__ == "__main__":
-    asyncio.run(mmp_main())
+    try:
+        asyncio.run(mmp_main())
+    except RuntimeError:
+        print("RE in __main__")
+
