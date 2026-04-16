b05d5ad5a58eb755ad9d3db8ff5262d93d6f8070
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-04-16 09:20:45 +0800

    Non-AIO version (not working yet)

--- simpleble/sb_example.py
+++ simpleble/sb_example.py
@@ -1,15 +1,41 @@
 import asyncio
-from simplepyble.aio import Adapter
+import sys
+
+#from simplepyble.aio import Adapter
+from simplepyble import Adapter
 
 async def main():
     adapters = Adapter.get_adapters()
     adapter = adapters[0]
+    target_device = None
 
-    async with adapter:
-        await adapter.scan_for(5000)
-        peripherals = adapter.scan_get_results()
-        for peripheral in peripherals:
-            print(f"Found: {peripheral.identifier()} [{peripheral.address()}]")
+    adapter.scan_for(5000)
+    peripherals = adapter.scan_get_results()
+    for peripheral in peripherals:
+        print(f"Found: {peripheral.identifier()} [{peripheral.address()}]")
+    target_device = [ p for p in peripherals if p.identifier() in sys.argv ][0]
+    print(f"Target: {target_device!r}")
+
+    target_device.connect()
+    await asyncio.sleep(10)
+    print("Successfully connected, listing services...")
+
+    if True:
+        # """
+        services = target_device.services()
+        service_characteristic_pair = []
+        for service in services:
+            for characteristic in service.characteristics():
+                service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
+
+        if not service_characteristic_pair:
+            print("No services or characteristics found.")
+            return
+        # """
+
+
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
