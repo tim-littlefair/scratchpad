b4939a7ec6a29c94745ac83f0240ca7e01a42dd4
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-04-16 09:22:06 +0800

    Subscribe TY.2b10 succeeds (no notifications yet)

--- maneline-mmp/pygatt_example.py
+++ maneline-mmp/pygatt_example.py
@@ -1,5 +1,13 @@
 import pygatt
 from binascii import hexlify
+import sys
+import time
+import traceback
+
+import logging
+
+logging.basicConfig()
+logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
 adapter = pygatt.GATTToolBackend()
 
@@ -8,14 +16,26 @@ def handle_data(handle, value):
     handle -- integer, characteristic read handle the data was received on
     value -- bytearray, the data returned in the notification
     """
-    print("Received data: %s" % hexlify(value))
+    print("Received data: %s" % hexlify(value),flush=True)
 
 try:
     adapter.start()
-    device = adapter.connect('01:23:45:67:89:ab')
+    device = adapter.connect(sys.argv[1], timeout=12)
 
-    device.subscribe("a1e8f5b1-696b-4e4c-87c6-69dfe0b0093b",
-                     callback=handle_data)
+    #for uuid in device.discover_characteristics().keys():
+    #    print("Read UUID %s: %s" % (uuid, hexlify(device.char_read(uuid))))
+
+    for uuid_arg in sys.argv[2:]:
+        op_in_progress = None
+        try:
+            if len(uuid_arg)==4:
+                uuid_arg=f"0000{uuid_arg}-0000-1000-8000-00805f9b34fb"
+            op_in_progress = "subscribing to "
+            device.subscribe(uuid_arg,callback=handle_data)
+            print("subscribe completed")
+        except:
+            print(f"Exception while {op_in_progress} {uuid_arg}:")
+            traceback.print_exc()
 
     # The subscription runs on a background thread. You must stop this main
     # thread from exiting, otherwise you will not receive any messages, and
@@ -23,7 +43,11 @@ try:
     # solution that won't eat up unnecessary CPU, but there are many other
     # ways to handle this in more complicated program. Multi-threaded
     # programming is outside the scope of this README.
-    while True:
-        time.sleep(10)
+    countdown = 30
+    while countdown>0:
+        time.sleep(1)
+        countdown-=1
+        print(f"\rCountdown: {countdown:2d}\r",end="",flush=True)
+
 finally:
     adapter.stop()
